2baf0916ac11fd195df973a32d645fece6be66ca
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-03-27 02:07:16 +0000

    initial commit of structure for TWM Website rebuild Q12009
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4657 7757fc27-82d5-0310-81f1-f107cb22aa12

---
+++ public/javascripts/application.js
@@ -0,0 +1,5 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+jQuery("#shell").corner("32px");
+jQuery("#shell .banner").corner("24px top");
