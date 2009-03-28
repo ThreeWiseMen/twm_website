4a4d0731519107bf14ae962e32ebeb1c9a1b0ed3
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-03-28 11:00:49 +0000

    making menu highlight follow page
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4677 7757fc27-82d5-0310-81f1-f107cb22aa12

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def sense_page(c, a)
+    if c == controller.controller_name and a == controller.action_name
+      "active"
+    end
+  end
+  
 end
