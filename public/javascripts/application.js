05e185f46544bec71d48607a34dacfe5aae4b504
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-03-28 15:15:34 +0000

    minor function name adjustment
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4683 7757fc27-82d5-0310-81f1-f107cb22aa12

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function init() {
+function page_init() {
 
     $("#shell").corner("28px");
-    $("#shell .banner").corner("25px tl tr");
+    $("#shell .banner").corner("22px tl tr");
 
-/*
-    $("#shell").corner({
-        tl: { radius: 28 },
-        tr: { radius: 28 },
-        bl: { radius: 28 },
-        br: { radius: 28 }
-    });
-
-    $("#shell .banner").corner({
-        tl: { radius: 25 },
-        tr: { radius: 25 }
-    })
-*/
     $("#tech_logos").cycle();
 
 }
