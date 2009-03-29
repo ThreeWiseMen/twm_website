8ad8ddbc04d00648bed21dce9ca75572a9287c31
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-03-29 03:26:20 +0000

    lighting up a simple article feed using ratom
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4686 7757fc27-82d5-0310-81f1-f107cb22aa12

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
 
+  require 'atom'
+
   #caches_page :index, :contact, :dev, :hosting, :success
+
+  FEED_URL = "http://svetzal.wordpress.com/category/business/feed/atom/"
+
+  def index
+    feed = Atom::Feed.load_feed(URI.parse(FEED_URL))
+    item = feed.entries.first
+    @title = item.title
+    @description = item.summary #methods.sort.join("<br />")
+    @link = item.links.first
+  end
+
+  def latest
+    feed = Atom::Feed.load_feed(URI.parse(FEED_URL))
+    item = feed.entries.first
+    @title = item.title
+    @content = item.content
+  end
 
 end
