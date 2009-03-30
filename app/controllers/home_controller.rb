14726235d79c205ea4c48df28610473552231242
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-03-30 15:13:51 +0000

    refixing hover issue in menu, reminifying, reactivating ratom in hopes I can make this actually work
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4700 7757fc27-82d5-0310-81f1-f107cb22aa12

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,31 @@
 class HomeController < ApplicationController
 
-#  require 'atom'
-#  require 'uri'
+  require 'atom'
+  require 'uri'
 
   caches_page :index, :contact, :dev, :hosting, :success
 
   FEED_URL = "http://svetzal.wordpress.com/category/business/feed/atom/"
 
   def index
-#    @blog_item = get_feed.entries.first
+    @blog_item = get_feed.entries.first
   end
 
   def blogentry
-#    id = "http://svetzal.wordpress.com/?p=#{params[:id]}"
-#    get_feed.entries.each do |entry|
-#      if entry.id == id
-#        @title = entry.title
-#        @content = entry.content
-#      end
-#    end
-#    redirect_to "/404.html" if @title.nil?
+    id = "http://svetzal.wordpress.com/?p=#{params[:id]}"
+    get_feed.entries.each do |entry|
+      if entry.id == id
+        @title = entry.title
+        @content = entry.content
+      end
+    end
+    redirect_to "/404.html" if @title.nil?
   end
 
   private
 
   def get_feed
-#    Atom::Feed.load_feed(URI.parse(FEED_URL))
+    Atom::Feed.load_feed(URI.parse(FEED_URL))
   end
 
 end
