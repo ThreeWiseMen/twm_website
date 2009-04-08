d9440ac78a647fedbfa806b211f45869ab1ff230
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-04-08 15:01:48 +0000

    adding in a tab interface, seeing how it looks
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4721 7757fc27-82d5-0310-81f1-f107cb22aa12

--- minify.sh
+++ minify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 yuicompressor public/javascripts/jquery-1.3.2.min.js > public/javascripts/all.min.js
+yuicompressor public/javascripts/jquery-ui-1.7.1.custom.min.js >> public/javascripts/all.min.js
 yuicompressor public/javascripts/jquery.cycle.all.min.js >> public/javascripts/all.min.js
 #yuicompressor public/javascripts/jquery.easing.1.3.js >> public/javascripts/all.min.js
 #yuicompressor public/javascripts/jquery.qtip-1.0.0-beta3.1.min.js >> public/javascripts/all.min.js
 yuicompressor public/javascripts/jquery.corner.js >> public/javascripts/all.min.js
 yuicompressor public/javascripts/application.js >> public/javascripts/all.min.js
 
-yuicompressor public/stylesheets/base.css > public/stylesheets/base.min.css
-
+yuicompressor public/stylesheets/base.css > public/stylesheets/all.min.css
+yuicompressor public/stylesheets/jquery-ui-1.7.1.custom.css >> public/stylesheets/all.min.css
