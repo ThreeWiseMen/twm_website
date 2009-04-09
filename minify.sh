9d3fae068f3b3a8ad6b9d91a9246f24c6c862bce
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2009-04-09 13:50:38 +0000

    making the tabs remember which was selected
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@4737 7757fc27-82d5-0310-81f1-f107cb22aa12

--- minify.sh
+++ minify.sh
@@ -2,6 +2,7 @@
 
 yuicompressor public/javascripts/jquery-1.3.2.min.js > public/javascripts/all.min.js
 yuicompressor public/javascripts/jquery-ui-1.7.1.custom.min.js >> public/javascripts/all.min.js
+yuicompressor public/javascripts/jquery.cookie.js >> public/javascripts/all.min.js
 yuicompressor public/javascripts/jquery.cycle.all.min.js >> public/javascripts/all.min.js
 #yuicompressor public/javascripts/jquery.easing.1.3.js >> public/javascripts/all.min.js
 #yuicompressor public/javascripts/jquery.qtip-1.0.0-beta3.1.min.js >> public/javascripts/all.min.js
