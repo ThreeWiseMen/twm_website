e3bab5c3b70010ba1743f1153e26efde2946c6a4
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2010-09-26 23:44:52 +0000

    preparing for rails3
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@5219 7757fc27-82d5-0310-81f1-f107cb22aa12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWMWebsite::Application.config.secret_token = 'dbcc2badb485bfecdaf5378daa091bece8b7846be27833f3956519ff605d9c93e125a875cf314c5922f3cb1e36dc77eb4dbbadc7020a7cca8d79f6d564db92d4'
