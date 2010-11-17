c47e04418dc940e6bd5fd1e700c82d72c36cbf2f
steve <steve@7757fc27-82d5-0310-81f1-f107cb22aa12>
2010-11-17 15:20:22 +0000

    updated to rails 3.0.3, logo system change
    git-svn-id: svn+ssh://svn.threewisemen.ca/home/svn/main/twm/head/TWM-Website@5223 7757fc27-82d5-0310-81f1-f107cb22aa12

--- minify.sh
+++ minify.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-yuicompressor public/javascripts/jquery-1.3.2.min.js > public/javascripts/all.min.js
-yuicompressor public/javascripts/jquery-ui-1.7.1.custom.min.js >> public/javascripts/all.min.js
-yuicompressor public/javascripts/jquery.cookie.js >> public/javascripts/all.min.js
-yuicompressor public/javascripts/jquery.cycle.all.min.js >> public/javascripts/all.min.js
-#yuicompressor public/javascripts/jquery.easing.1.3.js >> public/javascripts/all.min.js
-#yuicompressor public/javascripts/jquery.qtip-1.0.0-beta3.1.min.js >> public/javascripts/all.min.js
-yuicompressor public/javascripts/jquery.corner.js >> public/javascripts/all.min.js
-yuicompressor public/javascripts/application.js >> public/javascripts/all.min.js
+YUI=~/bin/yuicompressor
 
-yuicompressor public/stylesheets/base.css > public/stylesheets/all.min.css
-yuicompressor public/stylesheets/jquery-ui-1.7.1.custom.css >> public/stylesheets/all.min.css
+$YUI public/javascripts/jquery-1.3.2.min.js > public/javascripts/all.min.js
+$YUI public/javascripts/jquery-ui-1.7.1.custom.min.js >> public/javascripts/all.min.js
+$YUI public/javascripts/jquery.cookie.js >> public/javascripts/all.min.js
+$YUI public/javascripts/jquery.cycle.all.min.js >> public/javascripts/all.min.js
+#$YUI public/javascripts/jquery.easing.1.3.js >> public/javascripts/all.min.js
+#$YUI public/javascripts/jquery.qtip-1.0.0-beta3.1.min.js >> public/javascripts/all.min.js
+$YUI public/javascripts/jquery.corner.js >> public/javascripts/all.min.js
+$YUI public/javascripts/application.js >> public/javascripts/all.min.js
+
+$YUI public/stylesheets/base.css > public/stylesheets/all.min.css
+$YUI public/stylesheets/jquery-ui-1.7.1.custom.css >> public/stylesheets/all.min.css
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,19 @@
 
 function page_init() {
 
+    (function($) {
+      $.fn.randomize = function(childElem) {
+        return this.each(function() {
+          var $this = $(this);
+          var elems = $this.children(childElem);
+          elems.sort(function() { return (Math.round(Math.random())-0.5); });
+          $this.empty();
+          for(var i=0; i<elems.length; i++)
+            $this.append(elems[i]);
+        });
+      }
+    })(jQuery);
+
     if ($("#shell").length > 0) {
         $("#shell").corner("13px");
         if ($(".banner").length > 0)
@@ -13,6 +26,7 @@ function page_init() {
     }
 
     if ($("#tech_logos").length > 0) {
+        $("#tech_logos").randomize("img");
         $("#tech_logos").css('display', 'block');
         $("#tech_logos").cycle();
     }
