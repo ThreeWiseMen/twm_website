a1518e0ead98d791635400cfc9a4568f168955d6
Steven Vetzal <steve@vetzal.com>
2011-03-18 15:40:34 -0400

    update to deploy for ruby 1.9.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,14 +22,14 @@ role :app, "74.205.99.188"
 role :web, "74.205.99.188"
 role :db,  "74.205.99.188", :primary => true
 
-default_environment['PATH'] = "/home/twm/ruby-1.8.7/bin:/usr/local/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/CollabNet_Subversion/bin:/home/twm/bin"
+default_environment['PATH'] = "/home/twm/ruby-1.9.2/bin:/usr/local/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/CollabNet_Subversion/bin:/home/twm/bin"
 
 namespace :deploy do
   task :stop do
     run "cat #{deploy_to}/shared/pids/unicorn.pid* | xargs kill -s QUIT"
   end
   task :start do
-    run "cd #{current_path} && bundle exec $HOME/ruby-1.8.7/bin/unicorn_rails --config-file #{current_path}/config/unicorn.#{rails_env}.conf.rb --daemonize --env #{rails_env}"
+    run "cd #{current_path} && bundle exec $HOME/ruby-1.9.2/bin/unicorn_rails --config-file #{current_path}/config/unicorn.#{rails_env}.conf.rb --daemonize --env #{rails_env}"
   end
   task :restart do
     run "cat #{deploy_to}/shared/pids/unicorn.pid | xargs kill -s USR2 && sleep 3s && cat #{deploy_to}/shared/pids/unicorn.pid.oldbin | xargs kill -s QUIT"
