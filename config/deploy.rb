12bdd6de5f82d1570b2a3d09ddf70a57e72641c9
Steven Vetzal <steve@vetzal.com>
2011-03-18 17:37:34 -0400

    user rvm type

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require 'bundler/capistrano'
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                               # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2@twm'                      # Or whatever env you want it to run in.
+set :rvm_type, :user
 
 default_run_options[:pty] = true
 
