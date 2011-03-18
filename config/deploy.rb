c0b12657fc656af351a3471bbf2b82b75906a60b
Steven Vetzal <steve@vetzal.com>
2011-03-18 17:26:43 -0400

    preparing to have staging and production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,28 @@
 require 'bundler/capistrano'
 
-set :application, "TWM-Website"
-
 default_run_options[:pty] = true
 
-set :scm, :git
-set :user, 'twm'
+set :application, "TWM-Website"
 set :repository, 'git@github.com:ThreeWiseMen/twm_website.git'
-set :deploy_via, "remote_cache"
 
 set :deploy_to, "~/#{application}"
+
+set :scm, :git
+
+set :deploy_via, "remote_cache"
+
+set :user, 'twm'
 set :use_sudo, false
 
-set :branch, "production"
-set :rails_env, "production"
 
-role :app, "74.205.99.188"
-role :web, "74.205.99.188"
-role :db,  "74.205.99.188", :primary => true
-
-default_environment['PATH'] = "/home/twm/ruby-1.9.2/bin:/usr/local/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/home/twm/bin"
+task :production do
+  role :app, "74.205.99.188"
+  role :web, "74.205.99.188"
+  role :db,  "74.205.99.188", :primary => true
+  set :branch, "production"
+  set :rails_env, "production"
+  default_environment['PATH'] = "/home/twm/ruby-1.9.2/bin:/usr/local/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/home/twm/bin"
+end
 
 namespace :deploy do
   task :stop do
