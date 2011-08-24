340ce35f544f198c9cadfc6f4da1aa7f93e0cf68
Stacey Vetzal <stacey@vetzal.com>
2011-08-24 15:30:25 -0400

    change for production

--- config/unicorn.production.conf.rb
+++ config/unicorn.production.conf.rb
@@ -14,7 +14,7 @@ working_directory "#{APP_HOME}/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen '209.15.242.226:3000', :tcp_nopush => true
+listen '127.0.0.1:3000', :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 300
